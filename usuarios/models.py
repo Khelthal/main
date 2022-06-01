005fc6e18acfff4592978bb6c22f559f23a935d9
Juventino Aguilar Correa <juve@pop-os.localdomain>
2022-06-01 15:32:52 -0500

    Corregir error de matener varios usuarios: - Ahora la funcionalidad de tener varios usuarios que utilicen como base la clase User, funciona

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 
-TIPO = [
+TIPO_USUARIO = [
+    ('0', 'Invitado'), 
     ('1', 'Institucion Educativa'),
     ('2', 'Investigador'),
     ('3', 'Empresa'),
 ]
 
 class User(AbstractUser):
-    tipo_usuario = models.CharField('Tipo de Usuario', max_length=1, choices=TIPO)
+    tipo_usuario = models.PositiveSmallIntegerField(choices=TIPO_USUARIO)
 
 class InstitucionEducativa(models.Model):
-    user = models.OneToOneField(User, verbose_name="Usuario", related_name='datos', on_delete=models.DO_NOTHING)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
 class Investigador(models.Model):
-    user = models.OneToOneField(User, verbose_name="Usuario", related_name='datos', on_delete=models.DO_NOTHING)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
 class Empresa(models.Model):
-    user = models.OneToOneField(User, verbose_name="Usuario", related_name='datos', on_delete=models.DO_NOTHING)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
