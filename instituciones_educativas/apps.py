9bb4ecc11f8792c24ade1fefe2ef92512bf40252
AsuJuve <superjuve@outlook.es>
2022-06-07 14:58:22 -0500

    Se elimino la aplicacion instituciones_educativas

--- instituciones_educativas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InstitucionesEducativasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'instituciones_educativas'
