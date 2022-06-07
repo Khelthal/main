9450fc05b97aea35e68c6cf64fbc2640b39dd849
AsuJuve <superjuve@outlook.es>
2022-06-07 16:16:44 -0500

    Creacion del formulario de Tipo de Usuario

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from .models import TipoUsuario
+
 class UserForm(forms.ModelForm):
     repassword = forms.CharField()
     class Meta:
         model = User
         fields = ('username','password','email','repassword')
 
+class TipoUsuarioForm(forms.ModelForm):
+    class Meta:
+        model = TipoUsuario
+        fields = '__all__'
