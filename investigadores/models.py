c9047095a1997c370b9b68b092841992a1523a4d
Elias Beltran <eliasemilianobg@gmail.com>
2022-06-07 15:53:40 -0500

    Agregando columnas a modelos de usuarios

--- investigadores/models.py
+++ investigadores/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 from vinculacion.models import Categoria
-from usuarios.models import Investigador
+from usuarios.models import Ubicacion
+
+class Investigador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    ubicacion = models.ForeignKey(Ubicacion, on_delete=models.DO_NOTHING, blank=True, null=True)
+    nivel = models.ForeignKey(investigadores.NivelInvestigador, on_delete=models.DO_NOTHING)
+    curp = models.TextField()
 
 class Investigacion(models.Model):
     titulo = models.TextField()
     categorias = models.ManyToManyField(Categoria)
-    autores = models.ManyToManyField(Investigador)
+    autores = models.ManyToManyField(usuarios.Investigador)
     contenido = models.TextField()
+
+class NivelInvestigador(models.Model):
+    nivel = models.IntegerField()
+    descripcion = models.TextField()
--- usuarios/models.py
+++ usuarios/models.py
@@ -7,11 +7,16 @@ class TipoUsuario(models.Model):
 class User(AbstractUser):
     tipo_usuario = models.ForeignKey(TipoUsuario, on_delete=models.DO_NOTHING, null=True, blank=True)
 
+class Ubicacion(models.Model):
+    latitud = models.FloatField()
+    longitud = models.FloatField()
+
 class InstitucionEducativa(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-
-class Investigador(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    nombre_institucion = models.CharField(max_length=80)
+    ubicacion = models.ForeignKey(Ubicacion, on_delete=models.DO_NOTHING, blank=True, null=True)
 
 class Empresa(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    ubicacion = models.ForeignKey(Ubicacion, on_delete=models.DO_NOTHING, blank=True, null=True)
+    nombre_empresa = models.CharField(max_length=80)
