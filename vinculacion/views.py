2c1da3191283ed44cbda37f08c46050f72ef3fac
Elias Beltran <eliasemilianobg@gmail.com>
2022-06-07 15:01:35 -0500

    Mostrando categorias en el autocompletado

--- vinculacion/views.py
+++ vinculacion/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from vinculacion.models import Categoria
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
 def dashboard(request):
-    return render(request, "map.html")
+    categorias = Categoria.objects.all()
+    return render(request, "map.html", {"categorias":categorias})
