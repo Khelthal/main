46d5ec551892a0ad6697600a8900371deba28257
AsuJuve <superjuve@outlook.es>
2022-06-07 16:28:28 -0500

    Creacion del formulario del model de Invbestifacion

--- investigadores/forms.py
+++ investigadores/forms.py
@@ -1,10 +1,16 @@
 from attr import fields
 from django import forms
 
-from .models import Investigador
+from .models import Investigador, Investigacion
 
 class InvestigadorForm(forms.ModelForm):
     
     class Meta:
         model = Investigador 
         exclude = ['user']
+
+class InvestigacionForm(forms.ModelForm):
+    
+    class Meta:
+        model = Investigacion
+        fields = '__all__'
