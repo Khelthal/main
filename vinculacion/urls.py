4536da7a1c49ab7b77af0d59568693366612db25
Blackguz <36176398@uaz.edu.mx>
2022-06-07 21:15:25 -0500

    Se agrego la url de login en /usuarios/login, se agregaron los formularios y el css correspondiente

--- vinculacion/urls.py
+++ vinculacion/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
     path('dashboard', dashboard, name='dashboard'),
+    #Usuarios
+    path('usuarios/', include('usuarios.urls')),
 ]
