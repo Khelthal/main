266959d792960774a81362233662943d8d37f279
AsuJuve <superjuve@outlook.es>
2022-06-08 00:50:08 -0500

    Se separo el HTML del login, en login y registrar

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
 
+app_name = 'usuarios'
+
 urlpatterns = [
     path('login', login, name='login'),
+    path('registrar', registrar, name='registrar'),
 ]
