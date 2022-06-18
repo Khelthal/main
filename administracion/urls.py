610b6d8e4f896713d89410f47b93a8b201c33c20
Elias Beltran <eliasemilianobg@gmail.com>
2022-06-18 18:55:10 -0500

    CRUD de investigadores completado

--- administracion/urls.py
+++ administracion/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     #Investigadores
     path('investigadores/lista', investigadores_lista, name='investigadores_lista'),
     path('investigadores/nuevo', InvestigadorNuevo.as_view(), name='investigadores_nuevo'),
+    path('investigadores/editar/<int:pk>', InvestigadorEditar.as_view(), name='investigadores_editar'),
+    path('investigadores/eliminar/<int:pk>', InvestigadorEliminar.as_view(), name='investigadores_eliminar'),
     
     #Empresas
     path('empresas/lista', empresas_lista, name='empresas_lista'),
--- administracion/views.py
+++ administracion/views.py
@@ -4,8 +4,9 @@ from usuarios.models import User, TipoUsuario
 from investigadores.models import Investigador
 from empresas.models import Empresa
 from instituciones_educativas.models import InstitucionEducativa
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from administracion.forms import FormInvestigador
+from django.contrib import messages
 import requests
 import json
 
@@ -27,6 +28,7 @@ class InvestigadorNuevo(CreateView):
     form_class = FormInvestigador
     success_url = reverse_lazy('administracion:investigadores_lista')
     template_name = "administracion/investigadores_form.html"
+    extra_context = { "accion": "Crear" }
 
     def form_valid(self, form):
         investigador = form.save(commit=False)
@@ -42,11 +44,19 @@ class InvestigadorNuevo(CreateView):
         r = requests.get(f"https://nominatim.openstreetmap.org/search?street='{numero_exterior} {calle}'&city={municipio}&country=Mexico&state=Zacatecas&postalcode={codigo_postal}&format=json", headers=headers)
         
         if r.status_code != 200:
+            messages.error(self.request, "Error al obtener los datos de ubicación, por favor verifique que los datos de dirección ingresados son correctos.")
             return redirect('administracion:investigadores_nuevo')
 
-        data = json.loads(r.text)[0]
+        data = json.loads(r.text)
+        
+        if len(data) == 0:
+            messages.error(self.request, "Error al obtener los datos de ubicación, por favor verifique que los datos de dirección ingresados son correctos.")
+            return redirect('administracion:investigadores_nuevo')
+            
+        data = data[0]
 
         if "lat" not in data or "lon" not in data:
+            messages.error(self.request, "Error al obtener los datos de ubicación, por favor verifique que los datos de dirección ingresados son correctos.")
             return redirect('administracion:investigadores_nuevo')
         
         investigador.latitud = float(data["lat"])
@@ -56,8 +66,64 @@ class InvestigadorNuevo(CreateView):
         investigador.save()
         investigador.user.save()
 
+        messages.success(self.request, "Investigador registrado correctamente")
+        return redirect('administracion:investigadores_lista')
+
+class InvestigadorEditar(UpdateView):
+    model = Investigador
+    form_class = FormInvestigador
+    success_url = reverse_lazy('administracion:investigadores_lista')
+    template_name = "administracion/investigadores_form.html"
+    extra_context = { "accion": "Editar" }
+
+    def form_valid(self, form):
+        investigador = form.save(commit=False)
+        codigo_postal = form.cleaned_data['codigo_postal']
+        municipio = form.cleaned_data['municipio']
+        colonia = form.cleaned_data['colonia']
+        calle = form.cleaned_data['calle']
+        numero_exterior = form.cleaned_data['numero_exterior']
+        
+        headers = {
+            'User-agent': 'Script de consulta de ubicacion'
+        }
+        r = requests.get(f"https://nominatim.openstreetmap.org/search?street='{numero_exterior} {calle}'&city={municipio}&country=Mexico&state=Zacatecas&postalcode={codigo_postal}&format=json", headers=headers)
+        
+        if r.status_code != 200:
+            messages.error(self.request, "Error al obtener los datos de ubicación, por favor verifique que los datos de dirección ingresados son correctos.")
+            return redirect('administracion:investigadores_nuevo')
+
+        data = json.loads(r.text)
+        
+        if len(data) == 0:
+            messages.error(self.request, "Error al obtener los datos de ubicación, por favor verifique que los datos de dirección ingresados son correctos.")
+            return redirect('administracion:investigadores_nuevo')
+            
+        data = data[0]
+
+        if "lat" not in data or "lon" not in data:
+            messages.error(self.request, "Error al obtener los datos de ubicación, por favor verifique que los datos de dirección ingresados son correctos.")
+            return redirect('administracion:investigadores_nuevo')
+        
+        investigador.latitud = float(data["lat"])
+        investigador.longitud = float(data["lon"])
+        
+        investigador.save()
+
+        messages.success(self.request, "Investigador actualizado correctamente")
         return redirect('administracion:investigadores_lista')
         
+class InvestigadorEliminar(DeleteView):
+    model = Investigador
+    success_url = reverse_lazy('administracion:investigadores_lista')
+    template_name = "administracion/investigadores_confirm_delete.html"
+
+    def post(self, request, *args, **kwargs):
+        investigador = self.get_object()
+        investigador.user.tipo_usuario = None
+        investigador.user.save()
+
+        return self.delete(request, *args, **kwargs)
 
 def investigadores_nuevo(request):
     return render(request, "administracion/investigadores_nuevo.html")
