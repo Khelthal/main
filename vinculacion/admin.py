d7227fa5da16dcc8825d31a013e1208a6b553c20
AsuJuve <superjuve@outlook.es>
2022-06-19 20:51:13 -0500

    Se agrego el modelo de Noticia al sitio de admin

--- vinculacion/admin.py
+++ vinculacion/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from vinculacion.models import Categoria
+from vinculacion.models import Categoria, Noticia
 
 # Register your models here.
 admin.site.register(Categoria)
+admin.site.register(Noticia)
