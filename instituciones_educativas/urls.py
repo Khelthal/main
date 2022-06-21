33ecc4beab773cfcd297c45e1cf281ecc5c9eed7
Elias Beltran <eliasemilianobg@gmail.com>
2022-06-21 18:05:37 -0500

    Corrigiendo error en app_name

--- instituciones_educativas/urls.py
+++ instituciones_educativas/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from instituciones_educativas.views import InstitucionesEducativas
 
-app_name = 'investigadores'
+app_name = 'instituciones_educativas'
 
 urlpatterns = [
     path('fetch', InstitucionesEducativas.as_view(), name='instituciones_educativas_fetch'),
