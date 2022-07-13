9289075e702de1cdb20dc21ce047dd355b4aed8c
Elias Emiliano Beltran Gonzalez <eliasemilianobg@gmail.com>
2022-07-13 16:11:57 -0500

    Corrigiendo error al marcar el menú activo en el área de administración

--- static/js/administracion.js
+++
@@ -1,8 +0,0 @@
-const sideBarMenu = document.getElementById('sidebar-menu-container');
-function marcarMenuActivo(menu) {
-    Array.from(sideBarMenu.children).forEach((child) => {
-        if (child.children[0].textContent.trim().toLocaleLowerCase() == menu.toLocaleLowerCase()) {
-            child.classList.add("active");
-        }
-    });
-}
