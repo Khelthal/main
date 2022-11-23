da76e3ca2c550baeb4e6358945032bf4a4621a12
AsuJuve <superjuve@outlook.es>
2022-11-23 00:20:35 -0600

    Ahora se requiere la confirmación por correo electrónico para crear una cuenta

---
+++ usuarios/token.py
@@ -0,0 +1,11 @@
+from lib2to3.pgen2 import token
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+import six
+
+class Token(PasswordResetTokenGenerator):
+
+    # Usa el tiempo actual para generar el token (timestamp)
+    def _make_hash_value(self, user, timestamp):
+        return (six.text_type(user.pk) + six.text_type(timestamp)) + six.text_type(user.is_active)
+
+token_activacion = Token()
--- usuarios/views.py
+++ usuarios/views.py
@@ -5,6 +5,15 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth import get_user_model
 from django.contrib.messages.views import SuccessMessageMixin
 from .forms import UserForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from .token import token_activacion
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.views.generic import TemplateView
+from django.contrib import messages
+from django.shortcuts import render, redirect
 User = get_user_model()
 
 
@@ -15,14 +24,65 @@ class LoginView(LoginView):
 
 
 class RegistrarView(SuccessMessageMixin, CreateView):
-    template_name = 'auth/registrar.html'
     model = User
     form_class = UserForm
     success_url = reverse_lazy('usuarios:login')
-    success_message = "%(username)s se registró de manera exitosa"
+    template_name = 'auth/registrar.html'
+    success_message = "Un correo se le acaba de ser enviado para que pueda confirmar su correo electrónico, por favor verifiquelo para poder activar su cuenta. Recuerde revisar la carpeta de Spam"
 
     def form_valid(self, form):
-        user = form.save()
+        user = form.save(commit=False)
+        user.is_active = False
         user.save()
 
+        sitio = get_current_site(self.request)
+
+        uid = urlsafe_base64_encode(force_bytes(user.id))
+        token = token_activacion.make_token(user)
+        mensaje = render_to_string(
+            'confirmar_cuenta.html',
+            {
+                'user': user,
+                'sitio': sitio,
+                'uid': uid,
+                'token': token
+            }
+
+        )
+
+        mail_subject = 'Activar cuenta Sistema Estatal de Investigadores de Zacatecas'
+        mail_to = user.email
+        email = EmailMessage(
+            mail_subject,
+            mensaje,
+            to=[mail_to],
+        )
+
+        email.content_subtype = 'html'
+        email.send()
+
         return super().form_valid(form)
+
+
+class ActivarCuentaView(TemplateView):
+
+    def get(self, request, *args, **kwargs):
+
+        try:
+            uid = urlsafe_base64_decode(kwargs['uidb64'])
+            token = kwargs['token']
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, User.DoesNotExist):
+            user = None
+
+        if (user is not None) and token_activacion.check_token(user, token):
+            user.is_active = True
+            user.save()
+
+            messages.success(request, "Cuenta activada, ingresar datos")
+
+        else:
+            messages.error(
+                request, "Error al activar la cuenta, contacta al administrador")
+
+        return redirect('usuarios:login')
