604d2c9ce2ea979545411679b3b9630229b2f911
AsuJuve <superjuve@outlook.es>
2022-12-05 20:31:59 -0600

    Actualización a los nombres de pruebas unitarias

--- vinculacion/tests/tests_forms.py
+++ vinculacion/tests/tests_forms.py
@@ -14,73 +14,73 @@ class TestActualizarCuentaInvestigador(TestCase):
         self.client.login(
             username='prueba-form-investigador', password='12345')
 
-    def test_investigador_curp_incorrecto(self):
+    def test_actualizar_a_investigador_curp_incorrecto(self):
         datos = {'curp': 'incorrecto', 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos',
                  'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInvestigadorBase(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_investigador_codigo_postal_incorrecto(self):
+    def test_actualizar_a_investigador_codigo_postal_incorrecto(self):
         datos = {'curp': 'incorrecto', 'codigo_postal': '0', 'municipio': 'Jerez', 'colonia': 'Alamitos',
                  'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInvestigadorBase(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_investigador_numero_exterior_incorrecto(self):
+    def test_actualizar_a_investigador_numero_exterior_incorrecto(self):
         datos = {'curp': 'incorrecto', 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos',
                  'calle': 'Mezquite', 'numero_exterior': '-1', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInvestigadorBase(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_investigador_imagen_incorrecta(self):
+    def test_actualizar_a_investigador_imagen_incorrecta(self):
         datos = {'curp': 'incorrecto', 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos',
                  'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/incorrecto.txt'}
         form = FormInvestigadorBase(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_investigador_curp_vacio(self):
+    def test_actualizar_a_investigador_curp_vacio(self):
         datos = {'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos',
                  'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInvestigadorBase(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_investigador_codigo_postal_vacio(self):
+    def test_actualizar_a_investigador_codigo_postal_vacio(self):
         datos = {'curp': 'AUCJ011020HZSGRVA1', 'municipio': 'Jerez', 'colonia': 'Alamitos',
                  'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInvestigadorBase(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_investigador_municipio_vacio(self):
+    def test_actualizar_a_investigador_municipio_vacio(self):
         datos = {'curp': 'AUCJ011020HZSGRVA1', 'codigo_postal': '99390', 'colonia': 'Alamitos',
                  'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInvestigadorBase(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_investigador_colonia_vacia(self):
+    def test_actualizar_a_investigador_colonia_vacia(self):
         datos = {'curp': 'AUCJ011020HZSGRVA1', 'codigo_postal': '99390', 'municipio': 'Jerez',
                  'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInvestigadorBase(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_investigador_calle_vacia(self):
+    def test_actualizar_a_investigador_calle_vacia(self):
         datos = {'curp': 'AUCJ011020HZSGRVA1', 'codigo_postal': '99390', 'municipio': 'Jerez',
                  'colonia': 'Alamitos', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInvestigadorBase(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_investigador_numero_exterior_vacio(self):
+    def test_actualizar_a_investigador_numero_exterior_vacio(self):
         datos = {'curp': 'AUCJ011020HZSGRVA1', 'codigo_postal': '99390', 'municipio': 'Jerez',
                  'colonia': 'Alamitos', 'calle': 'Mezquite', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInvestigadorBase(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_investigador_acerca_de_vacio(self):
+    def test_actualizar_a_investigador_acerca_de_vacio(self):
         datos = {'curp': 'AUCJ011020HZSGRVA1', 'codigo_postal': '99390', 'municipio': 'Jerez',
                  'colonia': 'Alamitos', 'calle': 'Mezquite', 'numero_exterior': '29', 'imagen': '/tmp/foto.png'}
         form = FormInvestigadorBase(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_investigador_imagen_vacia(self):
+    def test_actualizar_a_investigador_imagen_vacia(self):
         datos = {'curp': 'AUCJ011020HZSGRVA1', 'codigo_postal': '99390', 'municipio': 'Jerez',
                  'colonia': 'Alamitos', 'calle': 'Mezquite', 'numero_exterior': '29'}
         form = FormInvestigadorBase(data=datos)
@@ -99,62 +99,62 @@ class TestActualizarCuentaEmpresa(TestCase):
         Categoria.objects.create(
             nombre='Cloud', area_conocimiento=area, descripcion='Chido')
 
-    def test_empresa_nombre_empresa_vacia(self):
+    def test_actualizar_a_empresa_nombre_empresa_vacia(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'especialidades': [especialidad], 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos',
                  'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormEmpresaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_empresa_especialidades_vacias(self):
+    def test_actualizar_a_empresa_especialidades_vacias(self):
         datos = {'nombre_empresa': 'Empresa juve', 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos',
                  'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormEmpresaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_empresa_codigo_postal_vacio(self):
+    def test_actualizar_a_empresa_codigo_postal_vacio(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_empresa': 'Empresa juve', 'especialidades': [
             especialidad], 'municipio': 'Jerez', 'colonia': 'Alamitos', 'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormEmpresaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_empresa_municipio_vacio(self):
+    def test_actualizar_a_empresa_municipio_vacio(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_empresa': 'Empresa juve', 'especialidades': [
             especialidad], 'codigo_postal': '99390', 'colonia': 'Alamitos', 'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormEmpresaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_empresa_nombre_colonia_vacio(self):
+    def test_actualizar_a_empresa_nombre_colonia_vacio(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_empresa': 'Empresa juve', 'especialidades': [
             especialidad], 'codigo_postal': '99390', 'municipio': 'Jerez', 'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormEmpresaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_empresa_nombre_calle_vacia(self):
+    def test_actualizar_a_empresa_nombre_calle_vacia(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_empresa': 'Empresa juve', 'especialidades': [
             especialidad], 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormEmpresaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_empresa_nombre_numero_exterior_vacio(self):
+    def test_actualizar_a_empresa_nombre_numero_exterior_vacio(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_empresa': 'Empresa juve', 'especialidades': [
             especialidad], 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos', 'calle': 'Mezquite', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormEmpresaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_empresa_nombre_acerca_de_vacio(self):
+    def test_actualizar_a_empresa_nombre_acerca_de_vacio(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_empresa': 'Empresa juve', 'especialidades': [
             especialidad], 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos', 'calle': 'Mezquite', 'numero_exterior': '29', 'imagen': '/tmp/foto.png'}
         form = FormEmpresaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_empresa_nombre_imagen_vacia(self):
+    def test_actualizar_a_empresa_nombre_imagen_vacia(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_empresa': 'Empresa juve', 'especialidades': [
             especialidad], 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos', 'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info'}
@@ -174,62 +174,62 @@ class TestActualizarCuentaInstitucion(TestCase):
         Categoria.objects.create(
             nombre='Cloud', area_conocimiento=area, descripcion='Chido')
 
-    def test_institucion_nombre_institucion_vacia(self):
+    def test_actualizar_a_institucion_nombre_institucion_vacia(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'especialidades': [especialidad], 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos',
                  'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInstitucionEducativaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_institucion_especialidades_vacias(self):
+    def test_actualizar_a_institucion_especialidades_vacias(self):
         datos = {'nombre_institucion': 'Institucion juve', 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos',
                  'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInstitucionEducativaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_institucion_codigo_postal_vacio(self):
+    def test_actualizar_a_institucion_codigo_postal_vacio(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_institucion': 'Institucion juve', 'especialidades': [
             especialidad], 'municipio': 'Jerez', 'colonia': 'Alamitos', 'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInstitucionEducativaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_institucion_municipio_vacio(self):
+    def test_actualizar_a_institucion_municipio_vacio(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_institucion': 'Institucion juve', 'especialidades': [
             especialidad], 'codigo_postal': '99390', 'colonia': 'Alamitos', 'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInstitucionEducativaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_institucion_nombre_colonia_vacio(self):
+    def test_actualizar_a_institucion_nombre_colonia_vacio(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_institucion': 'Institucion juve', 'especialidades': [
             especialidad], 'codigo_postal': '99390', 'municipio': 'Jerez', 'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInstitucionEducativaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_institucion_nombre_calle_vacia(self):
+    def test_actualizar_a_institucion_nombre_calle_vacia(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_institucion': 'Institucion juve', 'especialidades': [
             especialidad], 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos', 'numero_exterior': '29', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInstitucionEducativaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_institucion_nombre_numero_exterior_vacio(self):
+    def test_actualizar_a_institucion_nombre_numero_exterior_vacio(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_institucion': 'Institucion juve', 'especialidades': [
             especialidad], 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos', 'calle': 'Mezquite', 'acerca_de': 'Info', 'imagen': '/tmp/foto.png'}
         form = FormInstitucionEducativaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_institucion_nombre_acerca_de_vacio(self):
+    def test_actualizar_a_institucion_nombre_acerca_de_vacio(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_institucion': 'Institucion juve', 'especialidades': [
             especialidad], 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos', 'calle': 'Mezquite', 'numero_exterior': '29', 'imagen': '/tmp/foto.png'}
         form = FormInstitucionEducativaUpdate(data=datos)
         self.assertFalse(form.is_valid())
 
-    def test_institucion_nombre_imagen_vacia(self):
+    def test_actualizar_a_institucion_nombre_imagen_vacia(self):
         especialidad = Categoria.objects.get(nombre='Cloud')
         datos = {'nombre_institucion': 'Institucion juve', 'especialidades': [
             especialidad], 'codigo_postal': '99390', 'municipio': 'Jerez', 'colonia': 'Alamitos', 'calle': 'Mezquite', 'numero_exterior': '29', 'acerca_de': 'Info'}
